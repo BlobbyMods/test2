7b76f9e5838355647667b120c9b684506789413c
Danng <165156425+BlobbyMods@users.noreply.github.com>
2024-05-01 16:37:14 +0100

    Add files via upload

---
+++ src/main/java/org/cloudns/danng/test2.java
@@ -0,0 +1,17 @@
+package org.cloudns.danng;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class test2 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
